8f83624d31b36e9e6ee4b7137d30e96fac589a86
tran hau huan <huanth03@gmail.com>
2025-09-04 10:26:13 +0700

    first commit

---
+++ WeatherApiService/src/test/java/com/tranhuan/WeatherApiService/WeatherApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tranhuan.WeatherApiService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
