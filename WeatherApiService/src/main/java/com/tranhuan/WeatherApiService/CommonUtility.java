9acf39ed1661f06da73dd6df135a1d3083912b3b
tran hau huan <huanth03@gmail.com>
2025-09-16 23:35:33 +0700

    fix

--- WeatherApiService/src/main/java/com/tranhuan/WeatherApiService/CommonUtility.java
+++ WeatherApiService/src/main/java/com/tranhuan/WeatherApiService/CommonUtility.java
@@ -1,2 +1,29 @@
-package com.tranhuan.WeatherApiService;public class CommonUtility {
+package com.tranhuan.WeatherApiService;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CommonUtility {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CommonUtility.class);
+
+    public static String getIPAddress(HttpServletRequest request) {
+        String ip = null;
+
+        String xff = request.getHeader("X-Forwarded-For");
+        if (xff != null && !xff.isEmpty()) {
+            ip = xff.split(",")[0].trim();
+        }
+
+        if (ip == null || ip.isEmpty()) {
+            ip = request.getHeader("X-Real-IP");
+        }
+
+        if (ip == null || ip.isEmpty()) {
+            ip = request.getRemoteAddr();
+        }
+
+        LOGGER.info("Client's IP Address: {}", ip);
+        return ip;
+    }
 }
