e38e6393f6d46494cf3edc960406a61fd2b570d0
tran hau huan <huanth03@gmail.com>
2025-09-16 15:18:20 +0700

    validate request body

--- WeatherApiCommon/src/main/java/com/tranhuan/WeatherApiCommon/Location.java
+++ WeatherApiCommon/src/main/java/com/tranhuan/WeatherApiCommon/Location.java
@@ -1,5 +1,7 @@
 package com.tranhuan.WeatherApiCommon;
 
+import org.hibernate.validator.constraints.Length;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Column;
@@ -23,29 +25,31 @@ public class Location {
 
     @Column(length = 12, nullable = false, unique = true)
     @Id
-    @NotBlank
+    @NotNull(message = "Location code cannot be null")
+    @Length(min = 3, max = 12, message = "Location code must have 3-12 characters")
     private String code;
 
-    @NotBlank
     @Column(length = 128, nullable = false)
     @JsonProperty("city_name")
+    @NotNull(message = "City name cannot be null")
+    @Length(min = 3, max = 128, message = "City name must have 3-128 characters")
     private String cityName;
 
-
+    @Column(length = 128)
     @JsonProperty("region_name")
-    @NotNull
+    @Length(min = 3, max = 128, message = "Region name must have 3-128 characters")
     private String regionName;
-
 
     @Column(length = 64, nullable = false)
     @JsonProperty("country_name")
-    @NotBlank
+    @NotNull(message = "Country name cannot be null")
+    @Length(min = 3, max = 64, message = "Country name must have 3-64 characters")
     private String countryName;
-
 
     @Column(length = 2, nullable = false)
     @JsonProperty("country_code")
-    @NotBlank
+    @NotNull(message = "Country code cannot be null")
+    @Length(min = 2, max = 2, message = "Country code must have 2 characters")
     private String countryCode;
 
     private boolean enabled;
